0b36fda164fa99192303fe57dfcc1b8299ed114e
Bhatt <priya.bhatt@accenture.com>
2020-08-06 21:16:13 +0530

    sql_testcase.sql

--- sql_testcase.sql
+++ sql_testcase.sql
@@ -1,3 +1,12 @@
 select * from employees;
 
+--Alter table by adding new column to existing table
 
+alter table employees
+add column dept varchar(200);
+
+--Set default value to Column
+
+alter table employees
+alter dept set default 'abc';
+ 
