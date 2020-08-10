87e17794977df36a03b8d51a54aeb1657b57f786
Bhatt <priya.bhatt@accenture.com>
2020-08-10 19:25:38 +0530

    Readme updated

--- sql_testcase.sql
+++ sql_testcase.sql
@@ -9,4 +9,25 @@ add column dept varchar(200);
 
 alter table employees
 alter dept set default 'abc';
- 
+
+---View creation within table
+create view [supply details] as
+select supplierid,suppliername
+from suppliers
+
+---View creating using "where" condition
+create view [supply] as
+select supplierid,suppliername
+from suppliers
+where country = UK
+
+----Creating average View
+create view [prod avg price] as
+select productname, price
+from products
+where price >(select avg(price) from products)
+
+--view selection
+select * from [prod avg price]
+
+
