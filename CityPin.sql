b07a2b8b6e30f09d39573c533a33ab387480cefd
Priya2103 <69088521+Priya2103@users.noreply.github.com>
2022-01-13 13:10:37 +0530

    Add files via upload

---
+++ CityPin.sql
@@ -0,0 +1,23 @@
+select city, pincode
+from table_a
+order by city
+
+/*alter table table_a
+add pincode int(10)
+
+update table_a
+set pincode='560095'
+where city='bengaluru'
+
+update table_a
+set pincode='584115'
+where city='Raichuru'
+
+update table_a
+set pincode='5600086'
+where city='mysuru'
+
+alter table table_a
+drop pincode
+
+select * from table_a*/
